014cf1167aba6b2a41be32f2b7418786041519a3
Jamie Gardiner <jamie.gardiner1@gmail.com>
2024-10-20 16:03:24 +0100

    chore(packages): updated packages and tools

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  "*.ts": [
-    () => "npm run test:types",
-    "npm run lint:fix",
-    "npm run format:write",
-  ],
-  "*.{js,json,md}": ["npm run format:write"],
+	"*.{ts,js,json,md}": ["pnpm biome check --write"],
 };
